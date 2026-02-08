6b7b07de802bddb62627044f4093ce8c17ffd982
I343516 <darius.birescu@sap.com>
2026-02-08 17:49:53 +0200

    Update cleanup_old_results.sh

--- cleanup_old_results.sh
+++ cleanup_old_results.sh
@@ -2,5 +2,8 @@
 # Clean up old result files (not in folders)
 rm -f baseline_mocked_*.csv
 rm -f test_*_mocked_*.csv
+rm -f wrk_*_mocked_*.txt
+rm -f results_mocked.tex
+rm -f testfile.txt
 rm -rf results_mocked_*
 echo "✓ Cleaned up old result files"
